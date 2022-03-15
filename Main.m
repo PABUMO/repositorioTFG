aebf18c66a6702a3ea723b6d98d29520406668a0
Pablo Bustamante <pbustamante@estudiantec.cr>
2022-03-15 08:21:59 -0600

    Versión inicial del cálculo de la constancia de la forma del sonido.

--- Main.m
+++ Main.m
@@ -3,30 +3,32 @@ clear ; close all; clc;
 sonidoAIdentificar = "Sonidos/Pito de padre 1.ogg";
 [onda,frecuenciaDeMuestreo] = audioread(sonidoAIdentificar);
 
-graficarOndas(onda, frecuenciaDeMuestreo, 0);
+##graficarOndas(onda, frecuenciaDeMuestreo, 0);
 ##
-duracionDelSonido = calcularDuracionDelSonido(onda, frecuenciaDeMuestreo)
+##duracionDelSonido = calcularDuracionDelSonido(onda, frecuenciaDeMuestreo)
 ##
-if(duracionDelSonido >= 1) %Evaluación de la duración del sonido.
-  disp("El sonido dura más de 1 segundo");
-  [vectorFrecuencias, vectorEnergias] = calcularFFT(onda, frecuenciaDeMuestreo);
-  vectorEnergias = transpose(vectorEnergias); %Convertirla en un vector fila.
-  %Se calcula el rango de frecuencias con las energías más altas.
-  [valorInicialDelRangoPredominante, valorFinalDelRangoPredominante] = calcularRangoPredominante(vectorFrecuencias, vectorEnergias);
+##if(duracionDelSonido >= 1) %Evaluación de la duración del sonido.
+##  disp("El sonido dura más de 1 segundo");
+##  [vectorFrecuencias, vectorEnergias] = calcularFFT(onda, frecuenciaDeMuestreo);
+##  vectorEnergias = transpose(vectorEnergias); %Convertirla en un vector fila.
+##  %Se calcula el rango de frecuencias con las energías más altas.
+##  [valorInicialDelRangoPredominante, valorFinalDelRangoPredominante] = calcularRangoPredominante(vectorFrecuencias, vectorEnergias);
+##
+##  if(valorInicialDelRangoPredominante >= 2900 && valorFinalDelRangoPredominante <= 3150) %Evaluación de la frecuencia del sonido.
+##  disp("La frecuencia fundamental de la onda se encuentra dentro del rango de las bocinas");
+##    valorInicialDelRangoPredominante
+##    valorFinalDelRangoPredominante
+####    constanciaDeLaFormaDelSonido = calcularConstanciaDeLaFormaDelSonido(onda, frecuenciaDeMuestreo);
+####    if (constanciaDeLaFormaDelSonido >= 0.6)
+##      disp("El sonido es una bocina");
+####    else
+####      disp("El sonido no es una bocina");
+####    endif
+##  else
+##    disp("El sonido no es una bocina");
+##  endif
+##else
+##  disp("El sonido no es una bocina");
+##endif
 
-  if(valorInicialDelRangoPredominante >= 2900 && valorFinalDelRangoPredominante <= 3150) %Evaluación de la frecuencia del sonido.
-  disp("La frecuencia fundamental de la onda se encuentra dentro del rango de las bocinas");
-    valorInicialDelRangoPredominante
-    valorFinalDelRangoPredominante
-##    constanciaDeLaFormaDelSonido = calcularConstanciaDeLaFormaDelSonido(onda, frecuenciaDeMuestreo);
-##    if (constanciaDeLaFormaDelSonido >= 0.6)
-      disp("El sonido es una bocina");
-##    else
-##      disp("El sonido no es una bocina");
-##    endif
-  else
-    disp("El sonido no es una bocina");
-  endif
-else
-  disp("El sonido no es una bocina");
-endif
+calcularConstanciaDeLaFormaDelSonido(onda, frecuenciaDeMuestreo);
--- calcularConstanciaDeLaFormaDelSonido.m
+++ calcularConstanciaDeLaFormaDelSonido.m
@@ -1,3 +1,21 @@
-##function [out_valorDeConstanciaDeLaFormaDelSonido] = calcularConstanciaDeLaFormaDelSonido(in_onda, in_frecuenciaDeMuestreo)
-##  
-##endfunction
+function [out_valorDeConstanciaDeLaFormaDelSonido] = calcularConstanciaDeLaFormaDelSonido(in_onda, in_frecuenciaDeMuestreo)
+  
+  longitudDeLaOnda = length(in_onda)
+  
+  unidadesDeTiempo = linspace(0, longitudDeLaOnda/in_frecuenciaDeMuestreo, longitudDeLaOnda);
+  unidadesDeAmplitud = in_onda(1:longitudDeLaOnda);
+
+  valorPromedio = sum(unidadesDeAmplitud)/longitudDeLaOnda
+  umbral = valorPromedio*0.5
+  limiteSuperior = valorPromedio + umbral
+  limiteInferior = valorPromedio - umbral
+  
+  cantidadDeDatosEnElRango = 0;
+  for dato = unidadesDeAmplitud
+    if (limiteInferior <= dato && limiteSuperior >= dato)
+      cantidadDeDatosEnElRango++;
+    endif
+  endfor
+  cantidadDeDatosEnElRango
+  (cantidadDeDatosEnElRango/longitudDeLaOnda) * 100
+endfunction
