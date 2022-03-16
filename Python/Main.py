28d234d39dea2a2a7b07e1613b2f26e5fdf1c752
Pablo Bustamante <pbustamante@estudiantec.cr>
2022-03-15 22:15:17 -0600

    Inicio de la migración a Python.

--- Python/Main.py
+++ Python/Main.py
@@ -1,1 +1,17 @@
-print("Hola")
+import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
+import librosa as lr
+from glob import glob
+import soundfile
+
+onda, frecuenciaDeMuestreo = soundfile.read('../Sonidos/Pito de padre 1.wav')
+
+def graficarOndas(in_onda, in_frecuenciaDeMuestreo):
+    valoresDeTiempo = numpy.arange(0, len(onda))/frecuenciaDeMuestreo
+    fig, ax = plt.subplots()
+    ax.plot(valoresDeTiempo,onda)
+    ax.set(xlabel='Time(s)', ylabel='Sound Amplitud')
+    plt.show()
+
+graficarOndas(onda, frecuenciaDeMuestreo)
